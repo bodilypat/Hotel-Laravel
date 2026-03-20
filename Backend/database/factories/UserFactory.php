354c6f0e3a3875df9fcaea14450d97d5a2c3ecb3
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-20 10:16:12 -0700

    Add files via upload

--- Backend/database/factories/UserFactory.php
+++ Backend/database/factories/UserFactory.php
@@ -4,3 +4,26 @@
 <?php
 namespace Database\Factories;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'), // Default password for testing
+            'role' => $this->faker->randomElement(['admin', 'staff','customer']), // Randomly assign role
+            'remember_token' => \Str::random(10),
+        ];
+    }
+}
+
