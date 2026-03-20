e89dc4dff8486dad4628ce32aa9d7c77348a9b2f
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-20 10:07:43 -0700

    Delete frontend/src directory

--- frontend/src/pages/report/index.js
+++
@@ -1,8 +0,0 @@
-//src/pages/report/index.js
-
-export { default as AttendanceReport } from './AttendanceReport';
-export { default as LeaveReport } from './LeaveReport';
-export { default as PayrollReport } from './PayrollReport';
-export { default as EmployeeReport } from './EmployeeReport';
-export { default as RecruitReport } from './RecruitReport';
-
