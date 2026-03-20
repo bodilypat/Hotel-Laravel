d42882cf11bac632299a26e43cccfd3b2fffc4ff
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-20 13:16:52 -0700

    Create StoreBookingRequest.php

---
+++ Backend/app/Http/Requests/Booking/StoreBookingRequest.php
@@ -0,0 +1,34 @@
+<!-- app/Http/Requests/Booking/StoreBookingRequest.php 
+| -- 
+-->
+<?php
+
+namespace App\Http\Requests\Booking;
+
+use Illuminate\Foundation\Http\FormRequest;
+class StoreBookingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'room_id' => 'required|exists:rooms,id',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:start_time',
+        ];
+    }
+}
